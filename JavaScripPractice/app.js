82f6a52d38bcfacd70162749d7bd5160cd1c59f7
joonhwan1s <50772383+joonhwan1s@users.noreply.github.com>
2022-11-02 17:34:12 +0900

    20221102

---
+++ JavaScripPractice/app.js
@@ -0,0 +1,30 @@
+const password = prompt("please enter your PW")
+
+if(password.length >= 6){
+    console.log("logn")
+}else{
+    console.log("no")
+}
+
+
+
+// console.log("HELLO FROM OUR FIRST IS FILE!!!")
+// let total = 1 + 3;
+// console.log("GOOD BYE")
+
+
+// Math.random()
+// if(1){
+
+// }
+
+// const enter = prompt('Fuck you')
+
+// const mystery = 'P7pppp'; //CHANGE THIS VALUE TO MAKE THE CONDITIONAL BELOW TRUE
+
+
+
+// // LEAVE THIS CODE ALONE! (pretty please)
+// if(mystery[0] === 'P' && mystery.length > 5 && mystery.indexOf('7') !== -1){
+//     console.log("YOU GOT IT!!!");
+// }
