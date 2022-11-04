d9591be1afc017d17b541addf2bcaad0371387ec
joonhwan1s <50772383+joonhwan1s@users.noreply.github.com>
2022-11-04 18:00:52 +0900

    20221104

--- JavaScripPractice/app.js
+++ JavaScripPractice/app.js
@@ -1,10 +1,48 @@
-const password = prompt("please enter your PW")
+const numbers = [1,2,3,4,5,6,7,8,9]; //DON'T CHANGE THIS LINE PLEASE!
 
-if(password.length >= 6){
-    console.log("logn")
-}else{
-    console.log("no")
+// WRITE YOUR LOOP BELOW THIS LINE:
+
+for(let num of numbers){
+    console.log(num*num)
 }
+
+
+
+// let scores = [1,70,100, 2500, 9, -12, 0, 34]
+
+
+// const people = ["Scooby", "Velma", "Daphne", "Shaggy", "Fred"]; //DONT TOUCH THIS LINE!
+
+// WRITE YOUR LOOP BELOW THIS LINE:
+
+// for(let num = 0; num <= people.length-1; num++){
+//     console.log(people[num].toUpperCase());
+// }
+// const planets = ['The Moon','Venus', 'Earth', 'Mars', 'Jupiter']; //DO NOT TOUCH THIS LINE!
+
+// // YOUR CODE GOES BELOW THIS LINE:
+
+// planets.shift()
+
+// planets.push("Saturn")
+
+// planets.unshift("Mercury")
+
+// const leaderboard = ['Harry', 'Lua', 'Hermione', 'Bellatrix']; //DON'T TOUCH THIS LINE!
+
+// // YOUR CODE BELOW HERE:
+
+// leaderboard[1] = 'Luna'
+
+// leaderboard[3] = 'Draco'
+
+// const password = prompt("please enter your PW")
+
+// if(password.length >= 6){
+//     console.log("logn")
+// }else{
+//     console.log("no")
+// }
 
 
 
@@ -28,3 +66,4 @@ if(password.length >= 6){
 // if(mystery[0] === 'P' && mystery.length > 5 && mystery.indexOf('7') !== -1){
 //     console.log("YOU GOT IT!!!");
 // }
+
