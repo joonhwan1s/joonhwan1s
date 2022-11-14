f292bddd1e4d81efc791bf78ff05987c7f8a940c
joonhwan1s <50772383+joonhwan1s@users.noreply.github.com>
2022-11-14 22:29:03 +0900

    202211140

--- JavaScripPractice/DOM_Intro_CODE/JavaScripPractice/app.js
+++ JavaScripPractice/DOM_Intro_CODE/JavaScripPractice/app.js
@@ -1,7 +1,23 @@
 
 const tweetForm = document.querySelector('#tweetForm');
-
+const list = document.querySelector('#list');
 tweetForm.addEventListener('submit', function (e) {
     e.preventDefault();
-    
+    const usernameInput = tweetForm.elements.product;
+    const qtyInput = tweetForm.elements.qty;
+
+    usernameInput.value = '';
+    console.log(usernameInput.value);
+    qtyInput.value = '';
+
+
 });
+
+
+const addTweet = (usernameInput, qtyInput) => {
+    const newTweet = document.createElement('li');
+    const bTag = document.createElement('b');
+
+    
+
+}
