6f9fdd9b2a34daf98a6217ba9fa69311436427f0
joonhwan1s <50772383+joonhwan1s@users.noreply.github.com>
2022-12-08 17:32:49 +0900

    Npm

---
+++ Explore/app.js
@@ -0,0 +1,5 @@
+const { PI, square} = require('./math');// 같은 디렉토리 안에 있어서 경로를 하나만 써줌
+const cats = require('./shelter')
+
+// console.log(PI)
+console.log("REQQUIRE IN DIRECTOYU")
---
+++ Explore/math.js
@@ -0,0 +1,9 @@
+const add = (x,y) => x + y;
+
+const PI = 3.14159;
+
+const square = x => x * x;
+
+module.exports.add = "HELLO";
+module.exports.PI = PI;
+module.exports.square = square;
