cab11ac1ff3398b29920d2e362b0912bc057646c
joonhwan1s <50772383+joonhwan1s@users.noreply.github.com>
2022-12-11 15:33:23 +0900

    express project

--- Explore/Express_Intro/FirstApp/index.js
+++ Explore/Express_Intro/FirstApp/index.js
@@ -1,3 +1,29 @@
 const express = require("express");
 const app = express()
-console.dir(app)
+
+// app.use((req, res) =>{
+//     console.log("WE GOT A NEW REQUEST")
+//     res.send('<h1>This is my webpage</h1>')
+// })
+
+app.get('*', (req, res) => {
+    res.send('I DONT KNOW THAT')
+})
+
+app.get('/cats', (req, res) => {
+    res.send('MEOW')
+})
+
+app.post('/cats', (req,res) =>{
+    res.send("DDDDDDDDDDDDDD")
+})
+
+app.get('/dogs', (req,res) =>{
+    res.send('bark!')
+})
+// /cats => 'meow'
+// /
+
+app.listen(3000, () => {
+    console.log("LISTENING ON PORT 3000")
+})
